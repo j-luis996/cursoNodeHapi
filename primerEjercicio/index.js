e31068a21ef6d3e5e68bc8446829e591061dedb3
Luis Martinez <s_k_ap3@hotmail.com>
2021-01-21 18:04:43 -0600

    clase Uso de plugins - Contenido estático

--- primerEjercicio/index.js
+++ primerEjercicio/index.js
@@ -1,31 +1,44 @@
 'use strict'
 
 const Hapi = require('@hapi/hapi')
+const inert = require('@hapi/inert')
+const path = require('path')
+
 const config = require('./config')
 
 const server = Hapi.server({
       port: config.port,
       host: config.host,
+      routes: {
+            files: {
+                  relativeTo: path.join(__dirname,'public')
+            }
+      }
 })
 
 const init = async () => {
-      server.route({
-            method: 'GET',
-            path: '/',
-            handler: (req, h) => {
-                  return h.response('Hola mundo...').code(200)
-            }
-      })
-
-      server.route({
-            method: 'GET',
-            path: '/redirect',
-            handler: (req, h) => {
-                  return h.redirect('http://platzi.com')
-            }
-      })
-
       try{
+            await server.register(inert)
+
+            server.route({
+                  method: 'GET',
+                  path: '/home',
+                  handler: (req, h) => {
+                        return h.file('index.html')
+                  }
+            })
+      
+            server.route({
+                  method: 'GET',
+                  path: '/{param*}',
+                  handler: {
+                        directory: {
+                              path: '.',
+                              index: ['index.html']
+                        }
+                  }
+            })
+
             await server.start()
       }catch(err){
             console.error(err)
